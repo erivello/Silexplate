31baf17643c6c8997e677f7cdd13023484525f25
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-07-01 12:15:54 +0200

    monolog service

--- app/config/config.php
+++ app/config/config.php
@@ -2,9 +2,13 @@
 
 use Silex\Provider\HttpCacheServiceProvider;
 use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\MonologServiceProvider;
 
 // Cache dir
 $app['cache.path'] = __DIR__ . '/../cache';
+
+// Cache dir
+$app['logs.path'] = __DIR__ . '/../logs';
 
 // Debug
 $app['debug'] = true;
@@ -20,5 +24,12 @@ $app->register(new TwigServiceProvider(), array(
         'cache' => $app['cache.path'] . DIRECTORY_SEPARATOR . 'twig'
     ),
     'twig.path' => array(__DIR__ . '/../../src/views')
+));
+
+// Include Monolog to add application debug logging
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => $app['logs.path'] . DIRECTORY_SEPARATOR . 'app.log',
+    'monolog.name' => 'app',
+    'monolog.level' => 300 // = Logger::WARNING
 ));
 
