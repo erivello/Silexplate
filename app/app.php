5d267299b7f10d2715fa54d7841066ae26b41ccf
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-07-01 12:06:18 +0200

    twig service + small refactor

--- app/app.php
+++ app/app.php
@@ -2,12 +2,8 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use Silex\Provider\HttpCacheServiceProvider;
-
 $app = new Silex\Application();
 
 require __DIR__.'/config/config.php';
-
-$app->register(new HttpCacheServiceProvider());
 
 return $app;
--- app/config/config.php
+++ app/config/config.php
@@ -1,8 +1,24 @@
 <?php
 
+use Silex\Provider\HttpCacheServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+
+// Cache dir
+$app['cache.path'] = __DIR__ . '/../cache';
+
 // Debug
 $app['debug'] = true;
 
-// Cache
-$app['cache.path'] = __DIR__ . '/../cache';
-$app['http_cache.cache_dir'] = $app['cache.path'] . DIRECTORY_SEPARATOR . 'http';
+// Include HttpCache component for store the HTTP cache data
+$app->register(new HttpCacheServiceProvider(), array(
+    'http_cache.cache_dir' => $app['cache.path'] . DIRECTORY_SEPARATOR . 'http'
+));
+
+// Include Twig component for template rendering
+$app->register(new TwigServiceProvider(), array(
+    'twig.options' => array(
+        'cache' => $app['cache.path'] . DIRECTORY_SEPARATOR . 'twig'
+    ),
+    'twig.path' => array(__DIR__ . '/../../src/views')
+));
+
