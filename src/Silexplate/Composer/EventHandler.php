9492652d70ca00a944840bf5fed7d0bcb06d3249
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-07-02 16:59:50 +0200

    post installation process. set permissions of cache and logs folders

---
+++ src/Silexplate/Composer/EventHandler.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Silexplate\Composer;
+
+use Composer\Script\Event;
+
+/**
+ * EventHandler
+ */
+class EventHandler 
+{
+    /**
+     * Setting up permissions of cache and logs directories
+     * 
+     * @param Event $event 
+     */
+    public static function postInstall(Event $event)
+    {
+        chmod('app/cache', 0777);
+        chmod('app/logs',  0777);
+    }
+    
+}
