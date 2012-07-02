3cb49d4deff01e3eaf05fb55ef869a7d6b09eb33
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-07-02 15:57:08 +0200

    small refactor

--- app/app.php
+++ app/app.php
@@ -2,8 +2,31 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use Silex\Provider\HttpCacheServiceProvider;
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
+
+
 $app = new Silex\Application();
 
 require __DIR__.'/config/config.php';
+
+// Include Monolog to add application debug logging
+$app->register(new MonologServiceProvider(), $app['monolog.config']);
+
+// Include HttpCache component for store the HTTP cache data
+$app->register(new HttpCacheServiceProvider(), $app['http_cache.config']);
+
+// Include Session component to manage sessions
+$app->register(new SessionServiceProvider());
+
+// Include Twig component for template rendering
+$app->register(new TwigServiceProvider(), $app['twig.config']);
+
+// Include UrlGenerator component for generating URLs for named routes
+$app->register(new UrlGeneratorServiceProvider());
+
 
 return $app;
--- app/config/config.php
+++ app/config/config.php
@@ -1,43 +1,31 @@
 <?php
 
-use Silex\Provider\HttpCacheServiceProvider;
-use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\MonologServiceProvider;
-use Silex\Provider\UrlGeneratorServiceProvider;
-use Silex\Provider\SessionServiceProvider;
+// Debug
+$app['debug'] = true;
 
 // Cache dir
 $app['cache.path'] = __DIR__ . '/../cache';
 
-// Cache dir
+// Logs dir
 $app['logs.path'] = __DIR__ . '/../logs';
 
-// Debug
-$app['debug'] = true;
 
-// Include HttpCache component for store the HTTP cache data
-$app->register(new HttpCacheServiceProvider(), array(
+// HttpCache config
+$app['http_cache.config'] = array(
     'http_cache.cache_dir' => $app['cache.path'] . DIRECTORY_SEPARATOR . 'http'
-));
+);
 
-// Include Twig component for template rendering
-$app->register(new TwigServiceProvider(), array(
+// Monolog config
+$app['monolog.config'] = array(
+    'monolog.logfile' => $app['logs.path'] . DIRECTORY_SEPARATOR . 'app.log',
+    'monolog.name'    => 'app',
+    'monolog.level'   => 300 // = Logger::WARNING
+);
+
+// Twig config
+$app['twig.config'] = array(
     'twig.options' => array(
         'cache' => $app['cache.path'] . DIRECTORY_SEPARATOR . 'twig'
     ),
-    'twig.path' => array(__DIR__ . '/../../src/views')
-));
-
-// Include Monolog to add application debug logging
-$app->register(new MonologServiceProvider(), array(
-    'monolog.logfile' => $app['logs.path'] . DIRECTORY_SEPARATOR . 'app.log',
-    'monolog.name' => 'app',
-    'monolog.level' => 300 // = Logger::WARNING
-));
-
-// Include UrlGenerator component for generating URLs for named routes
-$app->register(new UrlGeneratorServiceProvider());
-
-// Include Session component to manage sessions
-$app->register(new SessionServiceProvider());
-
+    'twig.path'    => __DIR__ . '/../Resources/views'
+);
