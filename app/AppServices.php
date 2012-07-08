725866a15ebeda3dbd643584b7c1504bfc5418ee
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-07-08 23:14:14 +0200

    small refactor

---
+++ app/AppServices.php
@@ -0,0 +1,24 @@
+<?php
+
+use Silex\Provider\MonologServiceProvider;
+use Silex\Provider\HttpCacheServiceProvider;
+use Silex\Provider\SessionServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\UrlGeneratorServiceProvider;
+
+require __DIR__.'/config/config.php';
+
+// Include Monolog to add application debug logging
+$app->register(new MonologServiceProvider(), $app['monolog.config']);
+
+// Include HttpCache component for store the HTTP cache data
+$app->register(new HttpCacheServiceProvider(), $app['http_cache.config']);
+
+// Include Session component to manage sessions
+$app->register(new SessionServiceProvider());
+
+// Include Twig component for template rendering
+$app->register(new TwigServiceProvider(), $app['twig.config']);
+
+// Include UrlGenerator component for generating URLs for named routes
+$app->register(new UrlGeneratorServiceProvider());
--- app/app.php
+++ app/app.php
@@ -1,23 +1,9 @@
 <?php
 
-$app = new Silex\Application();
+use Silex\Application;
 
-require __DIR__.'/config/config.php';
+$app = new Application();
 
-// Include Monolog to add application debug logging
-$app->register(new Silex\Provider\MonologServiceProvider(), $app['monolog.config']);
-
-// Include HttpCache component for store the HTTP cache data
-$app->register(new Silex\Provider\HttpCacheServiceProvider(), $app['http_cache.config']);
-
-// Include Session component to manage sessions
-$app->register(new Silex\Provider\SessionServiceProvider());
-
-// Include Twig component for template rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), $app['twig.config']);
-
-// Include UrlGenerator component for generating URLs for named routes
-$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
-
+require __DIR__.'/AppServices.php';
 
 return $app;
