3c1713c6ae565f36d96c9bf2a70a35d17c11438b
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-07-13 18:12:01 +0200

    config refactor

--- app/services.php
+++ app/services.php
@@ -8,12 +8,6 @@ use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
 
 
-if (!file_exists(__DIR__.'/config/config.php')) {
-    throw new RuntimeException('You must create your own configuration file ("app/config/config.php"). See "app/config/config.php.dist" for an example config file.');
-}
-
-require __DIR__.'/config/config.php';
-
 // Include Monolog to add application debug logging
 $app->register(new MonologServiceProvider(), $app['monolog.config']);
 
